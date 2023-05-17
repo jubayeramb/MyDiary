0f827e29f29489d97ea39fff3d8eca8ba651a3d6
Jubayer Al Mamun <jubayeramb@gmail.com>
2023-05-17 15:12:09 +0600

    update(pages): login & signup page ui

--- src/GUI/pages/LoginPage.java
+++ src/GUI/pages/LoginPage.java
@@ -1,36 +1,97 @@
 package GUI.pages;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
+import GUI.components.MyButton;
+import GUI.components.MyInputField;
+import GUI.components.MyInputPasswordField;
 import data.interfaces.IAppNavigation;
 
 public class LoginPage extends JPanel {
     private IAppNavigation appNavigation;
+    private MyInputField usernameField;
+    private MyInputPasswordField passwordField;
+    private MyButton signupButton;
+    private MyButton loginButton;
+    private MyButton backButton;
 
     public LoginPage(IAppNavigation _appNavigation) {
         appNavigation = _appNavigation;
 
-        JLabel usernameLabel = new JLabel("Username:");
-        JTextField usernameField = new JTextField(20);
-        JLabel passwordLabel = new JLabel("Password:");
-        JPasswordField passwordField = new JPasswordField(20);
-        JButton loginButton = new JButton("Login");
-        loginButton.addActionListener(this::loginButtonActionPerformed);
+        setLayout(new BorderLayout());
 
-        add(usernameLabel);
-        add(usernameField);
-        add(passwordLabel);
-        add(passwordField);
-        add(loginButton);
+        // // Create constraints for center alignment
+        // GridBagConstraints constraints = new GridBagConstraints();
+        // constraints.gridx = 0;
+        // constraints.gridy = 0;
+        // constraints.weighty = 1.0;
+        // constraints.anchor = GridBagConstraints.CENTER;
+
+        // Create a panel for the signup form
+        JPanel logiPanel = new JPanel(new GridLayout(3, 2, -100, 10));
+        logiPanel.setBorder(new EmptyBorder(50, 0, 0, 0));
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new MyInputField();
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new MyInputPasswordField();
+
+        loginButton = new MyButton("Login");
+        signupButton = new MyButton("Signup");
+        backButton = new MyButton("Back");
+
+        logiPanel.add(usernameLabel);
+        logiPanel.add(usernameField);
+        logiPanel.add(passwordLabel);
+        logiPanel.add(passwordField);
+        logiPanel.add(new JLabel()); // Empty label for spacing
+        logiPanel.add(loginButton);
+
+        JPanel loginWrapper = new JPanel(new FlowLayout());
+        loginWrapper.add(logiPanel);
+
+        // Add the signup and back buttons to the bottom of the LoginPage
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.setBorder(new EmptyBorder(50, 0, 0, 0));
+        buttonPanel.add(signupButton);
+        buttonPanel.add(backButton);
+
+        JLabel loginLabel = new JLabel("Login");
+        loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        loginLabel.setFont(new Font("Arial", Font.BOLD, 36));
+        loginLabel.setBorder(new EmptyBorder(50, 0, 50, 0));
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBorder(new EmptyBorder(50, 50, 50, 50));
+        mainPanel.add(loginLabel, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        mainPanel.add(loginWrapper, BorderLayout.CENTER);
+
+        add(mainPanel, BorderLayout.CENTER);
+
+        // Add action listener to the signup button
+        loginButton.addActionListener(this::loginButtonActionPerformed);
+        signupButton.addActionListener(this::signupButtonActionPerformed);
+        backButton.addActionListener(this::backButtonActionPerformed);
     }
 
     private void loginButtonActionPerformed(ActionEvent e) {
         appNavigation.showMainAppPage();
+    }
+
+    private void signupButtonActionPerformed(ActionEvent e) {
+        appNavigation.showSignupPage();
+    }
+
+    private void backButtonActionPerformed(ActionEvent e) {
+        appNavigation.showWelcomePage();
     }
 }
--- src/GUI/pages/SignupPage.java
+++ src/GUI/pages/SignupPage.java
@@ -1,70 +1,151 @@
 package GUI.pages;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
+import GUI.components.MyButton;
+import GUI.components.MyInputField;
+import GUI.components.MyInputPasswordField;
 import data.interfaces.IAppNavigation;
 
 public class SignupPage extends JPanel {
     private IAppNavigation appNavigation;
-    private JTextField usernameField;
-    private JPasswordField passwordField;
-    private JButton signupButton;
+    private MyInputField firstNameField;
+    private MyInputField lastNameField;
+    private MyInputField emailField;
+    private MyInputField phoneField;
+    private MyInputPasswordField passwordField;
+    private MyInputPasswordField confirmPasswordField;
+    private MyButton signupButton;
+    private MyButton loginButton;
+    private MyButton backButton;
 
     public SignupPage(IAppNavigation _appNavigation) {
         appNavigation = _appNavigation;
 
-        setLayout(new GridBagLayout());
+        // setLayout(new GridBagLayout());
 
-        // Create constraints for center alignment
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.weighty = 1.0;
-        constraints.anchor = GridBagConstraints.CENTER;
+        // GridBagConstraints constraints = new GridBagConstraints();
+        // constraints.gridx = 0;
+        // constraints.gridy = 0;
+        // constraints.weighty = 1.0;
+        // constraints.anchor = GridBagConstraints.CENTER;
 
-        // Create a panel for the signup form
-        JPanel signupPanel = new JPanel(new GridLayout(3, 2, 10, 10));
-        JLabel usernameLabel = new JLabel("Username:");
-        usernameField = new JTextField(20);
+        setLayout(new BorderLayout());
+
+        JLabel signupLabel = new JLabel("Signup");
+        signupLabel.setFont(new Font("Arial", Font.BOLD, 36));
+        signupLabel.setHorizontalAlignment(JLabel.CENTER);
+        signupLabel.setBorder(new EmptyBorder(0, 0, 50, 0));
+
+        JPanel signupPanel = new JPanel(new GridLayout(8, 2, -100, 10));
+        JPanel signupPanelWrapper = new JPanel(new FlowLayout());
+
+        JLabel firstNameLabel = new JLabel("First Name:");
+        firstNameField = new MyInputField();
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        lastNameField = new MyInputField();
+        JLabel emailLabel = new JLabel("Email:");
+        emailField = new MyInputField();
+        JLabel phoneLabel = new JLabel("Phone:");
+        phoneField = new MyInputField();
         JLabel passwordLabel = new JLabel("Password:");
-        passwordField = new JPasswordField(20);
-        signupButton = new JButton("Signup");
-        signupPanel.add(usernameLabel);
-        signupPanel.add(usernameField);
+        passwordField = new MyInputPasswordField();
+        JLabel confirmPasswordLabel = new JLabel("Confirm Password:");
+        confirmPasswordField = new MyInputPasswordField();
+
+        signupButton = new MyButton("Signup");
+        loginButton = new MyButton("Login");
+        backButton = new MyButton("Back");
+
+        signupPanel.add(firstNameLabel);
+        signupPanel.add(firstNameField);
+        signupPanel.add(lastNameLabel);
+        signupPanel.add(lastNameField);
+        signupPanel.add(emailLabel);
+        signupPanel.add(emailField);
+        signupPanel.add(phoneLabel);
+        signupPanel.add(phoneField);
         signupPanel.add(passwordLabel);
         signupPanel.add(passwordField);
+        signupPanel.add(confirmPasswordLabel);
+        signupPanel.add(confirmPasswordField);
         signupPanel.add(new JLabel()); // Empty label for spacing
         signupPanel.add(signupButton);
 
-        // Add the signup panel to the center of the SignupPage
-        add(signupPanel, constraints);
+        signupPanelWrapper.add(signupPanel);
 
-        // Add action listener to the signup button
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.add(loginButton);
+        buttonPanel.add(new JLabel(""));
+        buttonPanel.add(backButton);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(signupLabel, BorderLayout.NORTH);
+        mainPanel.add(signupPanelWrapper, BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        mainPanel.setBorder(new EmptyBorder(50, 50, 50, 50));
+
+        add(mainPanel, BorderLayout.CENTER);
+
         signupButton.addActionListener(this::signupButtonActionPerformed);
+        loginButton.addActionListener(this::loginButtonActionPerformed);
+        backButton.addActionListener(this::backButtonActionPerformed);
+    }
+
+    public void clearFields() {
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");
+        phoneField.setText("");
+        passwordField.setText("");
+        confirmPasswordField.setText("");
+    }
+
+    public void setFocusOnFirstNameField() {
+        firstNameField.requestFocus();
+    }
+
+    public void setSignupButtonEnabled(boolean enabled) {
+        signupButton.setEnabled(enabled);
     }
 
     private void signupButtonActionPerformed(ActionEvent e) {
-        // Perform signup logic here
-        String username = usernameField.getText();
-        String password = new String(passwordField.getPassword());
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String email = emailField.getText();
+        String phone = phoneField.getText();
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
 
+        // Perform signup logic here
         // You can replace the print statements with your own logic
         System.out.println("Signup:");
-        System.out.println("Username: " + username);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
         System.out.println("Password: " + password);
+        System.out.println("Confirm Password: " + confirmPassword);
 
-        // After signup logic, navigate to the main app page
         appNavigation.showMainAppPage();
+    }
+
+    private void loginButtonActionPerformed(ActionEvent e) {
+        appNavigation.showLoginPage();
+    }
+
+    private void backButtonActionPerformed(ActionEvent e) {
+        appNavigation.showWelcomePage();
     }
 
 }
